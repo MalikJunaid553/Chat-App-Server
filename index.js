887909d5898a6c0e215c2065eed09db88aade78e
MalikJunaid553 <malikjunaidawan087@gmail.com>
2026-08-02 02:22:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,65 @@
-import express from "express"
+import express from "express";
 import http from "http";
-import {Server} from "socket.io"
+import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const server = http.createServer(app);
-const io  = new Server(server , {
-    cors:{
-        origin : "http://localhost:5173",
-        methods : ["GET" , "POST"],
-        credentials : true
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
     }
 });
 
-app.get("/", (req,res)=>{
-    res.send("<h1>Hello from Realtime Socket Chat App!!!</h1>")
-})
 
-io.on("connection", (socket)=>{
-    console.log("A new user Joined" , socket.id)
+// Needed for serving React build files
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-    socket.on("join" , (roomId)=>{
-        socket.join(roomId)
-    })
-    socket.on("leave" , (roomId)=>{
+
+// Serve React frontend
+app.use(express.static(path.join(__dirname, "client", "dist")));
+
+
+// Socket.IO
+io.on("connection", (socket) => {
+    console.log("A new user joined", socket.id);
+
+
+    socket.on("join", (roomId) => {
+        console.log(`${socket.id} joined ${roomId}`);
+        socket.join(roomId);
+    });
+
+
+    socket.on("leave", (roomId) => {
         socket.leave(roomId);
-    })
+    });
 
 
-    socket.on("send" , (message)=>{
-        socket.to(message.room).emit("message" , message);
+    socket.on("send", (message) => {
+        console.log("Message:", message);
+
+        socket.to(message.room).emit("message", message);
+    });
+
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id);
     });
 });
+
+
+// React fallback
+app.get("*", (req, res) => {
+    res.sendFile(
+        path.join(__dirname, "client", "dist", "index.html")
+    );
+});
+
 
 const PORT = process.env.PORT || 5050;
 
